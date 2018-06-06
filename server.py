cfdbf2b3384ba4b9adf79b2b9090da68705e245e
misterion <Ilyalas@gmail.com>
2018-06-06 12:33:43 +0300

    Screenshot is sent as a byte array now, no I/O operations(kind of)

--- server.py
+++ server.py
@@ -2,9 +2,17 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from os import curdir, sep
 import urllib.parse as urlparse
 import socket
-
+import io
 import pyautogui
 
+import mss
+import Xlib
+from PIL import Image
+
+resolution = Xlib.display.Display().screen().root.get_geometry()
+
+WIDTH = resolution.width
+HEIGHT = resolution.height
 
 PORT_NUMBER = 50000
 FILE_NAME = "screenshot.png"
@@ -42,8 +50,9 @@ class HTTPHandler(BaseHTTPRequestHandler):
 
         self.path = parsed.path
         mimetype = 'text/html'
+
         if self.path == "/" + FILE_NAME:
-            take_ss()
+            f = take_ss()
             mimetype = 'image/png'
         else:
             if self.path == "/":
@@ -57,13 +66,14 @@ class HTTPHandler(BaseHTTPRequestHandler):
                 mimetype = 'application/javascript'
             if self.path.endswith(".png"):
                 mimetype ='image/png'
+            file = open(curdir + sep + self.path, 'rb')
+            f = file.read()
+            file.close()
 
         self.send_response(200)
         self.send_header('Content-type', mimetype)
         self.end_headers()
-        f = open(curdir + sep + self.path, 'rb')
-        self.wfile.write(f.read())
-        f.close()
+        self.wfile.write(f)
         return
 
 def get_ip():
@@ -80,10 +90,18 @@ def get_ip():
 
 
 def take_ss():
-    pyautogui.screenshot(FILE_NAME)
+    with mss.mss() as sct:
+        rect = {'top': 0, 'left': 0, 'width': WIDTH, 'height': HEIGHT}
+        sct_img = sct.grab(rect)
 
+        byte_arr = io.BytesIO()
+        img = Image.frombytes('RGB', sct_img.size, sct_img.bgra, 'raw', 'BGRX')
+
+        img.save(byte_arr, format='PNG')
+    return byte_arr.getvalue()
 
 def main():
+    print("---Resoultion: {}x{}---".format(WIDTH, HEIGHT))
     server = None
     try:
         # Create a web server and define the handler to manage the
@@ -95,7 +113,6 @@ def main():
 
     except KeyboardInterrupt:
         print('---^C received, shutting down the web server---')
-        running = False
         server.socket.close()
 
 
